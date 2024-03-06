2d62516b9b4172452041d56d87c87d2321156aeb
jonathandemario <c14210001@john.petra.ac.id>
2024-03-06 14:13:23 +0700

    change link

--- sw.js
+++ sw.js
@@ -16,7 +16,7 @@ var STATIC_FILES = [
   '/src/css/app.css',
   '/src/css/feed.css',
   '/src/images/main-image.jpg',
-  '/node_modules/bootstrap/dist/css/bootstrap.rtl.min.css',
+  '/node_modules/bootstrap/dist/css/bootstrap.min.css',
   '/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js',
   '/node_modules/@popperjs/core/dist/umd/popper.min.js',
   '/node_modules/bootstrap/dist/js/bootstrap.min.js'
