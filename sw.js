e77fe3837b8e9d2e354f119c8b25184742ca3ec0
jonathandemario <c14210001@john.petra.ac.id>
2024-03-07 22:39:40 +0700

    new

--- sw.js
+++ sw.js
@@ -15,9 +15,10 @@ var STATIC_FILES = [
   '/src/js/material.min.js',
   '/src/css/app.css',
   '/src/css/feed.css',
-  'https://fonts.googleapis.com',
-  'https://fonts.gstatic.com',
-  'https://fonts.googleapis.com/css2?family=Tilt+Prism&display=swap'
+  '/src/images/main-image.jpg',
+  'https://fonts.googleapis.com/css?family=Roboto:400,700',
+  'https://fonts.googleapis.com/icon?family=Material+Icons',
+  'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
 ];
 
 // function trimCache(cacheName, maxItems) {
